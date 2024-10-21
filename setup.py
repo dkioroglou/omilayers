79c109c8da8cde664cd49055ae7543a413aab5db
Dimitrios Kioroglou <dkioroglou@lab-bioit6.cicbiogune.int>
2024-10-21 13:02:15 +0200

    Changed version to v0.2.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.2.1'
+VERSION = '0.2.2'
 DESCRIPTION = 'A SQLite and DuckDB wrapper suitable for bioinformatic analysis of multi-omic data.'
 LONG_DESCRIPTION = 'This package wraps the APIs of SQLite and DuckDB and provides a subset of their functionality that is suitable for frequent and repetitive tasks involved in bioinformatic analysis of multi-omic data.'
 
@@ -25,7 +25,7 @@ setup(
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
-    keywords=["duckdb", "omics", "bioinformatics", "data analysis"],
+    keywords=["duckdb", "sqlite3", "omics", "bioinformatics", "data analysis"],
     install_requires=read_file("requirements.txt"),
     packages=find_packages(),
     classifiers=[
